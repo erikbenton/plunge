9bb4bc6f87e5fe46ea4ed7bb043901623bf31332
Erik Benton <erik.benton@hotmail.com>
2026-01-13 10:39:12 -0800

    adding basic HTML sanitizing

--- utils/schemas.js
+++ utils/schemas.js
@@ -1,12 +1,35 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!"
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {}
+        });
+        if (clean !== value) return helpers.error("string.escapeHTML", { value });
+        return clean;
+      }
+    }
+  }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.diveSpotSchema = Joi.object({
   diveSpot: Joi.object({
-    title: Joi.string().required().min(3),
+    title: Joi.string().required().min(3).escapeHTML(),
     depth: Joi.number().required().min(0),
     //images: Joi.string().required(),
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
     longitude: Joi.number(),
     latitude: Joi.number()
   }).required(),
@@ -16,6 +39,6 @@ module.exports.diveSpotSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required()
+    body: Joi.string().required().escapeHTML()
   }).required()
 });
